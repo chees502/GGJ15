9cbaddbd16ca7052ada3685ac8270fa82f1f9659
Jacob Penner <jkpenner@live.com>
2015-01-23 23:53:12 -0800

    Initial Character Scripts
    The initial character controller and camera scripts

---
+++ Assets/_GGJ/Scripts/DogCharacterCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogCharacterCamera : MonoBehaviour {
+    public float cameraLookRate = 10f;
+    public Transform followTarget;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = followTarget.position;
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * cameraLookRate * Time.deltaTime);
+	}
+}
---
+++ Assets/_GGJ/Scripts/DogCharacterController.cs
@@ -0,0 +1,60 @@
+﻿#define Temp
+
+using UnityEngine;
+using System.Collections;
+
+public class DogCharacterController : MonoBehaviour {
+    public float CameraLookRate = 1f;
+
+    public Transform cameraTrans;
+
+    public float verticalAccel;
+    public float horizontalAccel;
+
+    public float maxForwardRightVel = 1000;
+
+    public float rotationRate = 10f;
+
+    void Awake() {
+
+    }
+
+    void Update()
+    {
+        // Get forward/right input
+        Vector3 moveVector = new Vector3(
+            Input.GetAxis("Horizontal") * horizontalAccel,
+            0f,           
+            Input.GetAxis("Vertical") * verticalAccel);
+
+        // Add forward/right input to velocity
+        rigidbody.velocity += transform.TransformDirection(moveVector);
+
+        // Limit the forward/right velocity
+        Vector2 frLimitied = new Vector2(rigidbody.velocity.x, rigidbody.velocity.z);
+        if (frLimitied.magnitude > maxForwardRightVel) {
+            frLimitied = frLimitied.normalized * maxForwardRightVel;
+        }
+        rigidbody.velocity = new Vector3(frLimitied.x, rigidbody.velocity.y, frLimitied.y);
+
+        // Rotates towards the camera's facing direction
+        // Only when the character is moving
+        if (moveVector.magnitude > 0) {
+            Quaternion targetRot = Quaternion.LookRotation((new Vector3(cameraTrans.forward.x, 0, cameraTrans.forward.z)).normalized);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationRate * Time.deltaTime);
+        }
+
+        // Handle Jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+    }
+
+    
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Trigger entered");
+    }
+
+
+}
