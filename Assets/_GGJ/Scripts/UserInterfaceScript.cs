00bb56ad4213ee16c0cc197d5daaf07588ce71a2
runetirius <d.yow@hotmail.com>
2015-01-23 23:43:59 -0800

    UserInterfaceScript Added
    Fixed Debug.Log in Test Scripts
    Added UserInterfaceScript.cs

---
+++ Assets/_GGJ/Scripts/UserInterfaceScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInterfaceScript : MonoBehaviour {
+
+    public GUIText Timer;
+    public float timeLeft;
+
+    void Update() {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            //Game over
+        }
+        else {
+            int minutes = (int)timeLeft / 60;
+            int seconds = (int)timeLeft % 60;
+            Timer.text = minutes + ":" + seconds;
+        }
+    }
+}
