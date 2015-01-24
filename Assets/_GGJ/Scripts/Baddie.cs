74576aba212e996fa2df7ea7e4084239d50cc22c
Brandon Farrell <chees502@gmail.com>
2015-01-24 00:03:55 -0800

    started first bad guy

---
+++ Assets/_GGJ/Scripts/Baddie.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baddie : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = target.position-transform.position;
+        float rotAmount = Vector3.Dot(transform.right, dir.normalized);
+        transform.Rotate(0, rotAmount * Time.deltaTime*50, 0);
+	}
+}
