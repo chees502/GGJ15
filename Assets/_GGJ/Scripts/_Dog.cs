c6d99a81203dd04b1bda60a444ab1d75b3f131b8
Jacob Penner <jkpenner@live.com>
2015-01-24 13:35:08 -0800

    Modify _Dog with Properties and Events
    Changed most public fields to be properties and added an event for when
    dog state chages

--- Assets/_GGJ/Scripts/_Dog.cs
+++ Assets/_GGJ/Scripts/_Dog.cs
@@ -1,13 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class _Dog : MonoBehaviour {
-    public static GameObject Dog;
-    public static DogCharacterCamera Camera;
-    public static DogCharacterController Controller;
-    public static Maw maw;
+public class _Dog {
     public enum _DogState { Idle, Climbing, Urinating };
-    public static _DogState DogState = _DogState.Idle;
+
+    public delegate void DogStateEvent(_DogState newState);
+
+    public static event DogStateEvent OnDogStateChange;
+
+    private static GameObject _dog;
+    private static DogCharacterCamera _camera;
+    private static DogCharacterController _controller;
+    private static _DogState _dogState = _DogState.Idle;
+
+    public static Maw maw;
+
+    public static GameObject Dog
+    {
+        get { return _dog; }
+        set { _dog = value; }
+    }
+
+    public static DogCharacterCamera Camera
+    {
+        get { return _camera; }
+        set { _camera = value; }
+    }
+
+    public static DogCharacterController Controller
+    {
+        get { return _controller; }
+        set { _controller = value; }
+    }
+
+    public static _DogState DogState
+    {
+        get { return _dogState; }
+        set
+        {
+            if (value != _dogState)
+            {
+                TriggerDogStateChangeEvent(value);
+            }
+            _dogState = value;
+        }
+    }
+
+    private static void TriggerDogStateChangeEvent(_DogState newState) {
+        if(OnDogStateChange != null) {
+            OnDogStateChange(newState);
+        }
+    }
+
 	// Use this for initialization
     public static void BuildDogConnection()
     {
@@ -17,12 +61,4 @@ public class _Dog : MonoBehaviour {
     {
         abberationBend.callTripping(time);
     }
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
