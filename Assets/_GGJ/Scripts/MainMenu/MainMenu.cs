dc678bfb9313d54e6a6c8840e4bc6db31fa817e9
Brandon Farrell <chees502@gmail.com>
2015-01-24 17:42:49 -0800

    Rigged pug

---
+++ Assets/_GGJ/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+    int w;
+    int h;
+    int hw;
+    int hh;
+	// Use this for initialization
+	void Start () {
+
+        w = Screen.width;
+        h = Screen.height;
+        hw = Mathf.FloorToInt(w * 0.5f);
+        hh = Mathf.FloorToInt(h * 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        GUI.Label(new Rect(hw - 50, 100, hw + 50, 50), "Pugs and Thugs");
+        if(GUI.Button(new Rect(hw-50,h-100,hw+50,h-50),"Play")){
+
+        }
+    }
+}
