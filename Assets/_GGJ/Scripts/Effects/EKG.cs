9378d987875a09e951308fa0b2c7b2d0ecba004c
Brandon Farrell <chees502@gmail.com>
2015-01-25 00:20:18 -0800

    Crap

--- Assets/_GGJ/Scripts/Effects/EKG.cs
+++ Assets/_GGJ/Scripts/Effects/EKG.cs
@@ -5,8 +5,10 @@ public class EKG : MonoBehaviour {
     public Texture2D surface;
     public Texture2D refrenceFG;
     public Texture2D flatLine;
+    public Renderer fadeBG;
     public Renderer sweepMat;
     public Camera castCam;
+    public float alpha = 0;
     Transform parent;
     public int index=0;
     public static bool flatLining = false;
@@ -17,10 +19,19 @@ public class EKG : MonoBehaviour {
         surface.wrapMode = TextureWrapMode.Clamp;
         renderer.material.SetTexture("_MainTex", surface);
         parent = transform.parent;
+        for (int x = 0; x < surface.width; x++)
+        {
+            for (int y = 0; y < surface.height; y++)
+            {
+                surface.SetPixel(x, y, Color.black);
+            }
+        }
+        surface.Apply();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
         for (int x = 0; x < 255; x++)
         {
@@ -39,7 +50,14 @@ public class EKG : MonoBehaviour {
         {
             parent.position = Vector3.Lerp(parent.position, Vector3.zero, Time.deltaTime*0.75f);
             parent.localScale = Vector3.Lerp(parent.localScale, new Vector3(4,4,4), Time.deltaTime * 0.25f);
-            if (Vector3.Distance(parent.position, Vector3.zero) < 0.1f)
+            Debug.Log(Time.deltaTime);
+            float dist = Vector3.Distance(parent.position, Vector3.zero);
+            if (dist < 0.25f)
+            {
+                alpha = Mathf.Lerp(alpha, 1.0f, Time.deltaTime);
+                fadeBG.material.SetColor("_Color",new Color(0,0,0,alpha));
+            }
+            if (dist < 0.1f && index % refrenceFG.width>240)
             {
                 refrenceFG = flatLine;
             }
