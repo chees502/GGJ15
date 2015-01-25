9dd3356b6136b619332683866e594227d3ea3713
Brandon Farrell <chees502@gmail.com>
2015-01-24 20:04:01 -0800

    EFG Timer Background

---
+++ Assets/_GGJ/Scripts/Effects/EKG.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EKG : MonoBehaviour {
+    public Texture2D surface;
+    public Texture2D refrenceFG;
+    public Renderer sweepMat;
+    public int index=0;
+	// Use this for initialization
+	void Start () {
+        surface = new Texture2D(600, 256);
+        surface.wrapMode = TextureWrapMode.Clamp;
+        renderer.material.SetTexture("_MainTex", surface);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int x = 0; x < 255; x++)
+        {
+            Color32 clr = refrenceFG.GetPixel(index%refrenceFG.width, x);
+            surface.SetPixel(index%600, x,clr);
+        }
+        surface.SetPixel(index % 600, 256, new Color32(0, 0, 0, 0));
+        sweepMat.material.SetTextureOffset("_MainTex", new Vector2(index*-.0016666f,0));
+        surface.Apply();
+        index++;
+	}
+}
--- Assets/_GGJ/Scripts/MainMenu/MainMenu.cs
+++ Assets/_GGJ/Scripts/MainMenu/MainMenu.cs
@@ -6,6 +6,8 @@ public class MainMenu : MonoBehaviour {
     int h;
     int hw;
     int hh;
+    public enum _menuState { MainMenu, Credits, Options };
+    public _menuState menuState = _menuState.MainMenu;
 	// Use this for initialization
 	void Start () {
 
@@ -17,13 +19,55 @@ public class MainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        w = Screen.width;
+        h = Screen.height;
+        hw = Mathf.FloorToInt(w * 0.5f);
+        hh = Mathf.FloorToInt(h * 0.5f);
 	}
     void OnGUI()
     {
+        switch (menuState)
+        {
+            case _menuState.MainMenu:
+                Main();
+                break;
+            case _menuState.Credits:
+                Credits();
+                break;
+            case _menuState.Options:
+                Options();
+                break;
+        }
+    }
+    void Main()
+    {
         GUI.Label(new Rect(hw - 50, 100, hw + 50, 50), "Pugs and Thugs");
-        if(GUI.Button(new Rect(hw-50,h-100,hw+50,h-50),"Play")){
-
+        if (GUI.Button(new Rect(hw - 50, h - 100, 100, 50), "Play"))
+        {
+            Application.LoadLevel("DebugScene");
+        }
+        if (GUI.Button(new Rect(w - 125, h - 100, 100, 50), "Credits"))
+        {
+            menuState = _menuState.Credits;
+        }
+        if (GUI.Button(new Rect(w - 100, 25, 50, 50), "☼"))
+        {
+            menuState = _menuState.Options;
+        }
+    }
+    void Credits()
+    {
+        if (GUI.Button(new Rect(25, 25, 50, 50), "◄"))
+        {
+            menuState = _menuState.MainMenu;
+        }
+    }
+    void Options()
+    {
+        if (GUI.Button(new Rect(25, 25, 50, 50), "◄"))
+        {
+            menuState = _menuState.MainMenu;
         }
     }
 }
