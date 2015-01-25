3d4a1d9882e0d71fdef141661ec8cf32ee362387
Jacob Penner <jkpenner@live.com>
2015-01-24 22:21:40 -0800

    Modify Consumable script
    Adds error handling and modifies the modifier to be an array of
    modifiers for multiple effects on one Consumable

--- Assets/_GGJ/Scripts/Consumables/Consumable.cs
+++ Assets/_GGJ/Scripts/Consumables/Consumable.cs
@@ -4,11 +4,29 @@ using System.Collections;
 public class Consumable : MonoBehaviour {
     public ParticleSystem onUseParticle;
     public AudioSource onUseAudio;
-    public ModType onUseModEnabled;
+
+    [System.Serializable]
+    public class Modifier {
+        public ModType type;
+        public float duration;
+    }
+
+    public Modifier[] onUseModEnabled;
+    
 
     void OnTriggerEnter(Collider other) {
-        onUseParticle.Play();
-        onUseAudio.Play();
-        ModManager.Instance.SetEffect(onUseModEnabled, true);
+        if (onUseParticle != null) {
+            onUseParticle.Play();
+        }
+
+        if (onUseAudio != null) {
+            onUseAudio.Play();
+        }
+
+        foreach (Modifier mod in onUseModEnabled) {
+            ModInfo info = ModManager.Instance.GetEffect(mod.type);
+            info.SetEnabled(true);
+            info.SetDuration(mod.duration);
+        }
     }
 }
