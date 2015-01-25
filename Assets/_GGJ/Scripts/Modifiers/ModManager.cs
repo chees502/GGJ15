0b7131d9debf30fb605b648d9c3cdb2ce043acd6
Jacob Penner <jkpenner@live.com>
2015-01-24 18:02:33 -0800

    Initial Modifier Scripts
    Initial ModManager, ModType, ModInfo scripts

---
+++ Assets/_GGJ/Scripts/Modifiers/ModManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ModManager : MonoBehaviour {
+    static private ModManager _instance;
+    const string _instanceName = "ModManager";
+
+    static public ModManager Instance {
+        get {
+            // If no ModManager instance try to find
+            // the Score Manager from the scene
+            if (_instance == null) {
+                var mmgo = GameObject.Find(_instanceName);
+                if (mmgo != null) {
+                    _instance = mmgo.GetComponent<ModManager>();
+                }
+            }
+
+            // If no ModManager instance create a new ModManager
+            if (_instance == null) {
+                var mmgo = new GameObject(_instanceName);
+                var mm = mmgo.AddComponent<ModManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private ModInfo[] _effects;
+
+    void Awake() {
+        if (_instance == null) {
+            _instance = this;
+        } else if(_instance != this) {
+            Debug.LogWarning("[ModManager]: More then one instance of the script in the scene");
+        }
+
+        _effects = new ModInfo[Enum.GetNames(typeof(ModType)).Length];
+        for (int i = 0; i < _effects.Length; i++) {
+            _effects[i] = new ModInfo();
+            _effects[i].Set(false);
+        }
+    }
+
+    void Start() {
+        ModInfo info = GetEffect(ModType.CameraEffect);
+        info.duration = 2f;
+        
+        SetEffect(ModType.CameraEffect, true);
+	}
+
+    void Update() {
+        for(int i = 0; i < _effects.Length; i++) {
+            _effects[i].Update();
+        }
+    }
+
+    public ModInfo GetEffect(ModType effect) {
+        if (_effects != null) {
+            return _effects[(int)effect];
+        }
+        return null;
+    }
+
+    public void SetEffect(ModType effect, bool enabled) {
+        ModInfo info = GetEffect(effect);
+        if (info != null) { 
+            info.Set(enabled);
+        }
+    }
+
+    public void ToggleEffect(ModType effect) {
+        ModInfo info = GetEffect(effect);
+        if (info != null) {
+            info.Toggle();
+        }
+    }
+}
---
+++ Assets/_GGJ/Scripts/Modifiers/ModType.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ModType {
+    DogShake = 0,
+    CameraShake = 1,
+    SpeedIncrease = 2,
+    CameraEffect = 3,
+    MusicEffect = 4,
+}
