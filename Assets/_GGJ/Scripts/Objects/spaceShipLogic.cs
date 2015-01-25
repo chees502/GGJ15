4029190b520039e05c8edb56ac1e9dbda9fcf1a3
Brandon Farrell <chees502@gmail.com>
2015-01-25 09:41:37 -0800

    Pugsly update

--- Assets/_GGJ/Scripts/Objects/spaceShipLogic.cs
+++ Assets/_GGJ/Scripts/Objects/spaceShipLogic.cs
@@ -8,12 +8,26 @@ public class spaceShipLogic : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         parent = transform.parent;
+        NewTarget();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         transform.Rotate(0, Time.deltaTime*20, 0);
         parent.position += parent.forward * Time.deltaTime*5;
         Vector3 dir = target - parent.position;
+        Debug.DrawLine(parent.position, target, Color.blue);
+        if (Vector3.Distance(parent.position, target) < 6)
+        {
+            NewTarget();
+        }
+        float turn = Vector3.Dot(parent.right, dir);
+        parent.Rotate(0, turn*Time.deltaTime, 0);
 	}
+    void NewTarget()
+    {
+        target.x = Random.Range(bound.min.x, bound.max.x);
+        target.z = Random.Range(bound.min.z, bound.max.z);
+    }
 }
