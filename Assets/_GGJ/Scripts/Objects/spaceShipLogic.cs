9d4bdf795b6733f0b2ea89dd42b2d0b07d5dccf9
Brandon Farrell <chees502@gmail.com>
2015-01-25 09:22:24 -0800

    UFO logic

---
+++ Assets/_GGJ/Scripts/Objects/spaceShipLogic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spaceShipLogic : MonoBehaviour {
+    Transform parent;
+    public Bounds bound;
+    Vector3 target;
+	// Use this for initialization
+	void Start () {
+        parent = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Time.deltaTime*20, 0);
+        parent.position += parent.forward * Time.deltaTime*5;
+        Vector3 dir = target - parent.position;
+	}
+}
