f6e5c1aab3ac0fb575aed544f4b512201ceb6ed5
Brandon Farrell <chees502@gmail.com>
2015-01-25 16:12:31 -0800

    Spawner

--- Assets/_GGJ/Scripts/Misc/Spawner.cs
+++ Assets/_GGJ/Scripts/Misc/Spawner.cs
@@ -7,16 +7,17 @@ public class Spawner : MonoBehaviour {
     public float radius = 10;
 	// Use this for initialization
 	void Start () {
-	
+
+        for (int x = 0; x < count; x++)
+        {
+            Vector2 pos2d = Random.insideUnitCircle;
+            Vector3 pos3d = new Vector3(pos2d.x * radius, 0, pos2d.y * radius);
+            Instantiate(GetOBJ(), pos3d, Random.rotation);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        for (int x = 0; x < count; x++)
-        {
-            Vector2 pos2d=Random.insideUnitCircle;
-            //Instantiate(GetOBJ(),Random.insideUnitCircle
-        }
 	}
     GameObject GetOBJ()
     {
