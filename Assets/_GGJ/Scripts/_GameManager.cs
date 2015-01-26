a82abb2df50006f7902e63c6b9f0e29de135dc27
nickvachara <vakara@hotmail.com>
2015-01-25 16:40:06 -0800

    jetpack behave

--- Assets/_GGJ/Scripts/_GameManager.cs
+++ Assets/_GGJ/Scripts/_GameManager.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class _GameManager : MonoBehaviour {
     public enum _gameMode {None ,ScoreAttack, FreeRoam }
     public static _gameMode gameMode = _gameMode.None;
-    public static Bounds SceneBound=new Bounds(Vector3.zero,new Vector3(40,20,40));
+    public static Bounds SceneBound=new Bounds(new Vector3(1120.0f, 100.0f, 1120.0f),new Vector3(2140,200,2140));
     public static bool isOculus = false;
 	// Use this for initialization
 	void Start () {
-	    
+	
 	}
 	
 	// Update is called once per frame
--- Assets/_GGJ/Scripts/jetPackBehave.cs
+++ Assets/_GGJ/Scripts/jetPackBehave.cs
@@ -18,8 +18,8 @@ public class jetPackBehave : MonoBehaviour {
 
 	float BezierTime = 0.0f;
 	int currentPoint;
-	public float accRate = 2.0f;
-	public float maxTravelSpeed = 80.0f;
+	public float accRate = 50.0f;
+	public float maxTravelSpeed = 500.0f;
 	public float travelSpeed = 0.0f;
 	public float turnSpeed = 2.0f;
 	Vector3 currentDest = new Vector3();
@@ -32,17 +32,20 @@ public class jetPackBehave : MonoBehaviour {
 	public ParticleSystem[] pSystems = new ParticleSystem[3];
 	// Use this for initialization
 	void Start () {
-		debugTime = Time.deltaTime;
+
 		pSystems = GetComponentsInChildren<ParticleSystem> ();
 		bound = _GameManager.SceneBound;
+	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		debugTime = Time.time;
 		if (testFlight) {
 			_current_state = _jetPack_states.touched;
 			testFlight = false;
 		}
+
 		StateManager ();
 	}
 
@@ -56,8 +59,9 @@ public class jetPackBehave : MonoBehaviour {
 		if (_current_state == _jetPack_states.touched) {
 			foreach (ParticleSystem ps in pSystems) {
 				ps.emissionRate = 120.0f;	
-				destStartTime = Time.time;
+
 			}
+			destStartTime = Time.time;
 			startPos = transform.position;
 			if(autoGenPoints){
 				createDestinationPoints(howManyPoints);
@@ -76,12 +80,17 @@ public class jetPackBehave : MonoBehaviour {
 		}
 		if (_current_state == _jetPack_states.endFlight) {
 			foreach (ParticleSystem ps in pSystems) {
-				ps.emissionRate = 0.0f;		
+				ps.emissionRate = 50.0f;
+				_Dog.DogState = _Dog._DogState.Idle;
 			}
 			returnBack();
 		}
 		if (_current_state == _jetPack_states.dead) {
+				foreach (ParticleSystem ps in pSystems) {
+					ps.emissionRate = 0.0f;		
+				}
 			travelSpeed = 0.0f;
+		//	accRate = 0.0f;
 
 		}
 	}
@@ -115,6 +124,12 @@ public class jetPackBehave : MonoBehaviour {
 		}
 	}
 
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "Player"){
+			_Dog.DogState = _Dog._DogState.ActionLock;
+			_current_state = _jetPack_states.touched;
+		}
+	}
 //	void bezierCalc( int points){
 //
 //		BezierTime = BezierTime + (Time.deltaTime * travelSpeed);
@@ -142,8 +157,8 @@ public class jetPackBehave : MonoBehaviour {
 	//	transform.position = Vector3.MoveTowards (transform.position, currentDest, travelSpeed * Time.deltaTime);
 		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (currentDest - transform.position), turnSpeed * Time.deltaTime);
 		transform.position += transform.forward * travelSpeed * Time.deltaTime;
-		if (Vector3.Distance (transform.position, currentDest) < 20.0f || Time.deltaTime > destStartTime+limitTime) {
-			destStartTime = Time.deltaTime;
+		if (Vector3.Distance (transform.position, currentDest) < 20.0f || Time.time > destStartTime+limitTime) {
+			destStartTime = Time.time;
 			currentPoint++;		
 		}
 		if (currentPoint >= howManyPoints) {
