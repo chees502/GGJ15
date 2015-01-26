5ff0beb71446b367b329f4b9c01c4df42b416a28
Brandon Farrell <chees502@gmail.com>
2015-01-25 16:45:34 -0800

    moved timer postion

--- Assets/_GGJ/Scripts/UserInterface/UserInterfaceScript.cs
+++ Assets/_GGJ/Scripts/UserInterface/UserInterfaceScript.cs
@@ -380,7 +380,7 @@ public class UserInterfaceScript : MonoBehaviour {
         ScoreManager.OnMultiplierChange += AddMultiplier;
         //Create Timer Object
         TimerObject = new GameObject("Timer");
-        TimerObject.transform.position = new Vector3(0.5f, 0.98f, 0.5f);
+        TimerObject.transform.position = new Vector3(0.5f, 0.50f, 0.5f);
         TimerObject.AddComponent("GUIText");
         TimerObject.guiText.anchor = TextAnchor.UpperCenter;
         TimerObject.guiText.alignment = TextAlignment.Center;
